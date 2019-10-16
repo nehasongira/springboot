25ea3a689040a0538c48ac3333f83daec5d87eed
nehasongira <songiraneha11@gmail.com>
2019-10-16 12:18:53 +0530

    nochange

--- src/main/java/com/stackroute/service/TrackDummyServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackDummyServiceImpl.java
@@ -1,5 +1,37 @@
 package com.stackroute.service;
 
-public class TrackDummyServiceImpl {
+import com.stackroute.domain.User;
+import com.stackroute.exceptions.TrackAlreadyExistsException;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
+@Profile("dummy")
+public class TrackDummyServiceImpl implements UserService {
+
+    @Override
+    public User saveUser(User user) throws TrackAlreadyExistsException {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(int id) {
+
+    }
+
+    @Override
+    public boolean UpdateComments(int trackId, String trackComments) {
+        return false;
+    }
+
+    @Override
+    public List<User> findTitleByName(String trackName) {
+        return null;
+    }
 }
--- src/main/java/com/stackroute/service/UserServiceImpl.java
+++ src/main/java/com/stackroute/service/UserServiceImpl.java
@@ -3,14 +3,38 @@ package com.stackroute.service;
 import com.stackroute.domain.User;
 import com.stackroute.exceptions.TrackAlreadyExistsException;
 import com.stackroute.repository.UserRepository;
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+
+//@Primary
+//@Profile({"dev", "prod"})
+//@PropertySource("classpath:application-prod.properties")
+
 public class UserServiceImpl implements UserService {
+
+//    @Value("${spring.datasource.url}")
+//    private String mysqlUrl;
+//
+//    //hello
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//
+//   @Autowired
+//    private Environment env;
+////
+//   String mongodbUrl = env.getProperty("spring.datasource.url");
+//   String defaultDb = env.getProperty("spring.datasource.username");
+
 
     UserRepository userRepository;
     @Autowired
