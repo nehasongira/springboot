738e59abb02ac47ee7c89801322ca2ae8b79394f
nehasongira <songiraneha11@gmail.com>
2019-10-16 09:46:59 +0530

    lombokdone

--- src/main/java/com/stackroute/domain/User.java
+++ src/main/java/com/stackroute/domain/User.java
@@ -1,85 +1,28 @@
 package com.stackroute.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "music")
 public class User {
-    public User(int id, String name, String singer, String film, String comments) {
-        this.id = id;
-        Name = name;
-        this.singer = singer;
-        this.film = film;
-        this.comments = comments;
-    }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", Name='" + Name + '\'' +
-                ", singer='" + singer + '\'' +
-                ", film='" + film + '\'' +
-                ", comments='" + comments + '\'' +
-                '}';
-    }
 
     @Id
     private int id;
     private String Name;
     private String singer;
     private String film;
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
     private String comments;
-
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public String getSinger() {
-        return singer;
-    }
-
-    public void setSinger(String singer) {
-        this.singer = singer;
-    }
-
-    public String getFilm() {
-        return film;
-    }
-
-    public void setFilm(String film) {
-        this.film = film;
-    }
-
-
-
-    public User() {
-    }
 
 
 
